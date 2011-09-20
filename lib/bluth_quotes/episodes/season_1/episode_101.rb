2674355bc4c5007cce22bc2d6aab900947bea985
Marcus Ortiz <mportiz08@gmail.com>
2011-09-19 18:19:24 -0700

    added quotes from episode 101

--- lib/bluth_quotes/episodes/season_1/episode_101.rb
+++ lib/bluth_quotes/episodes/season_1/episode_101.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'bluth_quotes/episode'
 
 module BluthQuotes
@@ -9,6 +10,19 @@ module BluthQuotes
           george_michael "Breakfast!"
           michael        "...no. Family."
           george_michael "Oh right! I thought you meant in the things you eat."
+        end
+        quote do
+          michael "I thought Buster had everything under control. I thought you've been going in to the office."
+          buster  "Yes, and I've enjoyed that. It's just that I was constantly being called to the phone, or I was asked a question, or I was being resuscitated, and it was really hard to get a good work flow going."
+        end
+        quote do
+          michael "Okay, guys, um... they are going to keep Dad in prison at least until this gets all sorted out. Also, the attorney said that they’re going to have to put a halt on the company’s expense account. [The others gasp.] Interesting. I would’ve expected that after \"They’re keeping Dad in jail.\""
+        end
+        quote do
+          gob "Illusion, Michael. A trick is something a whore does for money. [GOB notices children in earshot] ... Or candy!"
+        end
+        quote do
+          gob "Not tricks, Michael, illusions. A trick is something a whore does for money. [GOB notices children in earshot] ... Or cocaine!"
         end
       end
     end
