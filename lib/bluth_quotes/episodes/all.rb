0a1e91d03643d9543c035ab1b8f3292603e4c0de
Marcus Ortiz <mportiz08@gmail.com>
2011-09-19 21:20:01 -0700

    fixed small bug

--- lib/bluth_quotes/episodes/all.rb
+++ lib/bluth_quotes/episodes/all.rb
@@ -1,1 +1,1 @@
-Dir['bluth_quotes/episodes/season_1/*.rb'].each {|ep| require File.join("bluth_quotes/episodes/season_1/", File.basename(ep, '.rb'))}
+Dir["#{File.join(File.dirname(__FILE__), 'season_1')}/*.rb"].each {|ep| require File.join("bluth_quotes/episodes/season_1/", File.basename(ep, '.rb'))}
